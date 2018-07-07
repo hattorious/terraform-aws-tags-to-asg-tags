a2dffe0077db9994df947cb53f774d0903669a5b
Ross Hattori <git@exstasis.net>
2018-07-07 18:27:18 -0500

    doc: README and comments

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# terraform-aws-tags-to-asg-tags
-map("key1", "value1") => list(map("key", "key1", "value", "value1", "propagate_at_launch", true),)
+# Terraform AWS tags to AWS AutoScaling Group (ASG) tags
+Converts a [Terraform](https://www.terraform.io) map of `${length(n)}` into a list of [AWS AutoScaling Group tags](https://www.terraform.io/docs/providers/aws/r/autoscaling_group.html) which will propogate at launch.
+
+AWS ASGs have a different tagging structure which can cause pain when implementing a tagging policy; this module allows you to use the same map of tags on ASGs and other AWS resources.
+
+## Usage
+```HCL
+module "aws_asg_tags" {
+  source = "git@github.com:mavin/terraform-aws-tags-to-asg-tags.git"
+
+  tags = "${map(
+    "key1", "value1",
+    "key2", "value2",
+  )}"
+}
+
+...
+
+resource "aws_autoscaling_group" "foo" {
+
+  ...
+
+  tags = "${module.aws_asg_tags.tags}"
+}
+```
+
+Tested on Terraform v0.11.7 and `terraform-provider-null_v1.0.0`
--- vars.tf
+++ vars.tf
@@ -1,3 +1,4 @@
+// I'm abusing a few Terraform constructs here
 variable "tags" {
   type    = "map"
   default = {}
@@ -9,9 +10,17 @@ module "count" {
 }
 
 data "null_data_source" "tag" {
+  // In some cases, wrapping a count interpolation in a module pushes the
+  // interpolation forward in the graph so that Terraform calculates it earlier
+  // in the plan. This doesn't always work and will not work with if the
+  // interpolation has a dependency on managed resources.
+  // https://github.com/hashicorp/terraform/issues/12570#issuecomment-318414280
   count = "${module.count.count}"
 
+  // null_data_source exports the `inputs` and `outputs` attributes as the declared
+  // map so we can use it to create a map of known keys to interpolated values.
   inputs = {
+    // the keys() and values() interpolations return lists in the same order
     key                 = "${element(keys(var.tags), count.index)}"
     value               = "${element(values(var.tags), count.index)}"
     propagate_at_launch = "true"
@@ -19,5 +28,6 @@ data "null_data_source" "tag" {
 }
 
 output "tags" {
+  // splat collects up the attributes in a list
   value = "${data.null_data_source.tag.*.outputs}"
 }
